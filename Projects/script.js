2ba662060e8790b6856976358ef4f8da91c5d533
rezocoder <81382487+rezocoder@users.noreply.github.com>
2021-11-03 23:11:44 +0400

    Add files via upload

---
+++ Projects/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    $('.list').click(function() {
+        const value = $(this).attr('data-filter');
+        if (value == 'all') {
+            $('.itembox').show('1000');
+        }
+        else{
+            $('.itembox').not('.'+value).hide('1000');
+            $('.itembox').filter('.'+value).show('1000')
+        }
+    })
+
+    $('.list').click(function() {
+        $(this).addClass('active').siblings().removeClass('active')
+    })
+})
